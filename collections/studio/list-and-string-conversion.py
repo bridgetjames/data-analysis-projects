077d5c9ffeace5df1edc81fabc1dc3c99fbb16bd
bridgetjames <bridgetgjames@gmail.com>
2024-10-13 16:26:31 -0500

    collections studio

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -6,18 +6,38 @@ proto_list4 = "Comma-spaces, might, require, typing, caution"
 strings = [proto_list1, proto_list2, proto_list3, proto_list4]
 
 # a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
-
+# if ',' or ';' or ' ' in proto_list1:
+#     print("True")
+# else:
+#     print("False")
+# if ',' or ';' or ' ' in proto_list2:
+#     print("True")
+# else:
+#     print("False")
+# if ',' or ';' or ' ' in proto_list3:
+#     print("True")
+# else:
+#     print("False")
+# if ',' or ';' or ' ' in proto_list4:
+#     print("True")
+# else:
+#     print("False")    
 
 # b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
-
-
+new_proto_list1 = proto_list1.split(',')
+new_reversed_list1 = new_proto_list1[::-1]
+final_list1 = ','.join(new_reversed_list1)
+print(final_list1)
 
 # c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
-
-
+new_proto_list2 = proto_list2.split(';')
+sorted_list2 = sorted(new_proto_list2)
+final_list2 = ';'.join(sorted_list2)
+print(final_list2)
 
 # d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
-
-
+new_proto_list3 = proto_list3.split(' ')
+new_proto_list3.sort(reverse=True)
+print(new_proto_list3)
 
 # e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -2,14 +2,21 @@ my_string = "LaunchCode"
 
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
-
-
+new_string = my_string[3:]+my_string[0:3]
+print(new_string)
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
-
-
+# print("The original string reads as", my_string, "and the modified string reads as", new_string)
+print("The original string reads as {0}, and the modified string reads as {1}.".format(my_string, new_string))
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
+num_letters_relocated = input('User Entry: ')
+print(num_letters_relocated)
+# use this to convert string to integer: int(num_letters_relocated)
+new_string = my_string[int(num_letters_relocated):]+my_string[0:int(num_letters_relocated)]
 
 # c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if int(num_letters_relocated) > 10:
+    print('User input cannot be longer than word.')
+else:
+    print(new_string)
